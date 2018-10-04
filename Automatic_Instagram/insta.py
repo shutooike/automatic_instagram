27650d2dda9e24b8dc82e039bfc2fe57a2c8a6db
Shuto Oike <shutofootball@icloud.com>
2018-10-04 14:59:31 +0900

    add json

---
+++ Automatic_Instagram/insta.py
@@ -0,0 +1,141 @@
+# import
+import json
+import datetime
+import schedule
+import requests
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+
+# open and load json
+json_file = open('config.json')
+json_obj  = json.load(json_file)
+
+# assign value
+username          = json_obj['username']
+password          = json_obj['password']
+line_notify_token = json_obj['lineToken']
+hashtagList       = json_obj['hashtagList']
+specifiedTime     = json_obj['time']
+
+# url INFO
+instagramURL = "https://www.instagram.com"
+loginURL = "https://www.instagram.com/accounts/login/"
+profileURL = "https://www.instagram.com/{}/".format(username)
+
+# defining xpaths and selectors
+likeXpath = '/html/body/div[3]/div/div[2]/div/article/div[2]/section[1]/span[1]/button/span' #いいねボタン
+searchXpath = '//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/input' #検索
+numberOfFollowersXpath = '//*[@id="react-root"]/section/main/div/header/section/ul/li[2]/a/span'
+nextPageSelector = 'a.coreSpriteRightPaginationArrow' #次へボタン
+postSelector = 'div._9AhH0' #投稿の画像部分のselector
+buttonXpath = '/html/body/div[3]/div/div/div/div[3]/button[1]'
+
+# defining linenotify
+def linenotify(message):
+    line_notify_api = 'https://notify-api.line.me/api/notify'
+    payload = {'message': message}
+    headers = {'Authorization': 'Bearer ' + line_notify_token}
+    line_notify = requests.post(line_notify_api, data=payload, headers=headers)
+
+def like():
+    # post list
+    postlist = []
+
+    # total likes
+    totalLikes = 0
+
+    # job
+    job = 0
+    while job < 5:
+        # starting a new browser session
+        browser = webdriver.Chrome()
+
+        # login
+        browser.get(loginURL)
+        browser.implicitly_wait(10)
+        browser.find_element_by_name("username").send_keys(username)
+        browser.find_element_by_name("password").send_keys(password,Keys.RETURN)
+
+        # push button
+        sleep(3)
+        browser.implicitly_wait(10)
+        browser.find_element_by_xpath(buttonXpath).click()
+
+        # getting number of followers
+        sleep(3)
+        browser.get(profileURL)
+        followers = browser.find_element_by_xpath(numberOfFollowersXpath)
+        dt_now = datetime.datetime.now()
+        followersReport = "You have {} followers at {}".format(followers.text,dt_now)
+        message = '{}'.format(followersReport)
+        linenotify(message)
+
+        # hashtag loop
+        for hashtagName in hashtagList:
+            sleep(3)
+            browser.get(instagramURL)
+            sleep(3)
+
+        # searching for the hashtag
+            browser.implicitly_wait(10)
+            browser.find_element_by_xpath(searchXpath).send_keys("#{}".format(hashtagName))
+            sleep(5)
+            browser.find_element_by_xpath(searchXpath).send_keys(Keys.RETURN,Keys.RETURN)
+
+        # making a postList
+            sleep(5)
+            browser.implicitly_wait(10)
+            postList = browser.find_elements_by_css_selector(postSelector)
+
+        # automatic like
+            for post in postList:
+                sleep(3)
+                post.click()
+                nextCounter = 0
+                likedCounter = 0
+
+                while nextCounter < 9:
+                    sleep(2)
+                    browser.implicitly_wait(10)
+                    browser.find_element_by_css_selector(nextPageSelector).click()
+                    nextCounter += 1
+
+                while likedCounter < 50:
+                    try:
+                        sleep(3)
+                        browser.implicitly_wait(10)
+                        browser.find_element_by_xpath(likeXpath).click()
+                        browser.implicitly_wait(10)
+                        likedCounter += 1
+                        browser.find_element_by_css_selector(nextPageSelector).click()
+                    except:
+                        break
+
+                break
+
+            totalLikes += likedCounter
+            interimReportList = ["\n"]
+            interimReportList.append("liked {} posts of #{}\n".format(likedCounter,hashtagName))
+            continue
+
+        # report
+        finalReport = "\nliked {} posts".format(totalLikes)
+        interimReport = "".join(interimReportList)
+        message = '{}'.format(interimReport + finalReport)
+        linenotify(message)
+        sleep(3)
+
+        # clean exit
+        browser.close()
+        job += 1
+        sleep(1800)
+
+
+schedule.every().day.at(specifiedTime).do(like)
+
+while True:
+    schedule.run_pending()
+    sleep(1)
